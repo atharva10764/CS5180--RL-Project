07ee187ac255d86320c63e8ceb08ed92d15a81fa
Atharva Zadbuke <zadbuke.atharva18@siesgst.ac.in>
2024-04-25 19:39:21 -0400

    final Commit

---
+++ simulation/settings.py
@@ -0,0 +1,23 @@
+'''
+All the parameters used in the Simulation has been documented here.
+
+
+Names of the parameters are self-explanatory therefore elimating the use of further comments.
+'''
+
+
+HOST = "localhost"
+PORT = 2000
+TIMEOUT = 10.0
+
+CAR_NAME = 'BP_Volkswagen_T2_2021_RLW'
+EPISODE_LENGTH = 120
+NUMBER_OF_VEHICLES = 30
+NUMBER_OF_PEDESTRIAN = 10
+CONTINUOUS_ACTION = True
+VISUAL_DISPLAY = True
+
+
+RGB_CAMERA = 'sensor.camera.rgb'
+SSC_CAMERA = 'sensor.camera.semantic_segmentation'
+
